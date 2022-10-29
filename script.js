32ac7c83d1c99e3df2e5258db661afb9eba77197
camille <c.miyashita@gmail.com>
2022-10-28 23:52:12 -0300

    cores e chat ok

--- script.js
+++ script.js
@@ -41,20 +41,36 @@ function pegarMensagens(mensagem){
 
         for (let i = 0; i < mensagens.length; i++){
     const mensagem = mensagens[i];
-    if(mensagem.type) {
+    if(mensagem.type === "status") {
         chat.innerHTML += `
-        <div class="status">
-        <div class="time">(${mensagem.time})</div>
-        <div class="from">(${mensagem.from})</div>
-        <div class="text">(${mensagem.text})</div>
+        <div class="status ${mensagem.type}">
+        <div class="time"> (${mensagem.time}) </div>
+        <div class="from"> <strong>${mensagem.from}</strong> </div>
+        <div class="text"> ${mensagem.text} </div>
         </div>`
+    } else if (mensagem.type === "message") {
+        chat.innerHTML += `
+        <div class="status ${mensagem.type}">
+        <div class="time"> (${mensagem.time}) </div>
+        <div class="from"> <strong>${mensagem.from}</strong> para <strong>${mensagem.to}:</strong> ${mensagem.text}  </div>
+       
+        </div>
+        `
+    } else if (mensagem.type === "private_message" && mensagem.to == usuario) {
+        chat.innerHTML += `
+        <div class="mensagem-reservada ${mensagem.type}">
+        <div class="time"> (${mensagem.time}) </div>
+        <div class="from"> <strong>${mensagem.from}</strong> reservadamente para <strong>${mensagem.to}:</strong> ${mensagem.text} </div>
+        <div class="text"> ${mensagem.text} </div>
+        </div>
+        `
     }
         }
     }).catch((error) => {
         console.log(error);
     });
 
-   
+   chat.scrollIntoView();
 }
 
 
@@ -66,6 +82,12 @@ function pegarMensagens(mensagem){
      *      3.1 - puxar API pro projeto
      *      3.2 - pegar as respostas da API para o chat
      *      3.3 - puxar o chat para o projeto
+     * 
+     * Separar as mensagens de sair entrar por type
+Separar msg normal
+Separar msg privada
+Setar msg privada from - from
+Antes modificar css (ver classes novas)
      
 
 
